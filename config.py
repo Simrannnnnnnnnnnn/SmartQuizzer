116ed8b214b191eeba352eb0748d2627696d0970
Simran Kaur <95494098+Simrannnnnnnnnnnn@users.noreply.github.com>
2026-04-25 12:58:07 +0530

    fix: Switch to PostgreSQL via env variable

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'smart_quizzer_dev_key_99'
     
     
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
